2852356d23f0dc2f95c3e944da9553f78f939e1a
vsasidharan <105060540+vsasidharan@users.noreply.github.com>
2022-07-13 13:05:38 +0530

    new feature 2 added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Dummy = () =>{
     console.log('1.process');
     console.log('2.process');
-    console.log('new feature')
+    console.log('new feature');
+    console.log('new feature2');
 }
